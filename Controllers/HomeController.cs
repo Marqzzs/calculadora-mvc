b726ac9c60f72e8ba4c2d1330b8e0a4314b05f35
Mauricio <maumarqz007@gmail.com>
2023-05-23 09:51:01 -0300

    primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Nova_pasta.Models;
+
+namespace Nova_pasta.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Index(int numero1, int numero2)
+    {
+        int resultado = numero1 + numero2;
+        ViewBag.Resultado = resultado;
+
+        return View();
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
